f39927c4382dcb12a9cc0cad65c663d273d7a9ef
Yann Leblond <knt4xyz@gmail.com>
2022-03-16 11:47:45 +0100

    📝 reverse rows in table  js_function On

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 console.log('indexMain.js')
 
-document.getElementById(`1noGaranty_Img`).addEventListener('mouseover', function(e) {
-    console.log(`hello from mouseover`)
-    const redcross = document.getElementById(`redcroosId`)
-    redcross.classList(`.displayBckGrnd`)
+
+document.getElementById('arrowCol1').addEventListener('click', function(){
+
+
+    const tBodies = document.getElementsByTagName('tbody');
+    // console.log("file: pageContrat_1.js ~ line 35 ~ document.getElementById ~ tbody", tbody);
+    // new Array(tbody).reverse();
+    const newTbody = document.createElement('tbody');
+    const oldTbody = tBodies[0];
+    rows = oldTbody.rows,
+    i = rows.length - 1;
+
+    while (i >= 0) {
+        newTbody.appendChild(rows[i]);
+        i -= 1;
+    }
+    oldTbody.parentNode.replaceChild(newTbody, oldTbody);
+})
+
+
+document.getElementById('arrowCol6').addEventListener('click', function(){
+
+    console.log('testFunction');
+
+    const tBodies = document.getElementsByTagName('tbody');
+    // console.log("file: pageContrat_1.js ~ line 35 ~ document.getElementById ~ tbody", tbody);
+    // new Array(tbody).reverse();
+    const newTbody = document.createElement('tbody');
+    const oldTbody = tBodies[0];
+    rows = oldTbody.rows,
+    i = rows.length - 1;
+
+    while (i >= 0) {
+        newTbody.appendChild(rows[i]);
+        i -= 1;
+    }
+    oldTbody.parentNode.replaceChild(newTbody, oldTbody);
 })
